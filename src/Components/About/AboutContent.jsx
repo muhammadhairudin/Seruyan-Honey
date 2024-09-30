649cd9852ebdb46463f6eb1974489cec6e8d8489
portofolio-muhammadhairudin <abu.muhammadhairudin13@gmail.com>
2024-09-30 09:00:43 +0700

    perubahan data

--- src/Components/About/AboutContent.jsx
+++ src/Components/About/AboutContent.jsx
@@ -1,20 +1,19 @@
 // aboutContent.js
 
-// Exporting content for About Us page
+// tentang kami
 export const companyOverview = {
     title: "Siapa Kami",
     description: `
-      Kami adalah perusahaan yang berdedikasi untuk menyediakan produk madu berkualitas tinggi yang 100% alami. 
-      Berdiri sejak 2010, kami terus berupaya untuk menjaga keaslian dan kualitas madu yang kami produksi, 
-      dengan berkomitmen terhadap proses produksi yang alami dan berkelanjutan.
+      Kami berdedikasi untuk menyediakan produk madu berkualitas tinggi yang 100% alami dan murni. 
+      Kami terus berupaya untuk menjaga keaslian dan kualitas madu kami, 
+      dengan berkomitmen terhadap proses produksi pengemasan yang alami tanpa pemanasan.
     `,
   };
   
   export const missionStatement = {
     title: "Misi Kami",
     description: `
-      Misi kami adalah untuk memperkenalkan manfaat madu alami kepada masyarakat luas dan menjadi pemimpin pasar 
-      dalam penyediaan madu berkualitas tinggi. Kami percaya bahwa produk alami memiliki kekuatan untuk meningkatkan 
+      Misi kami adalah untuk memperkenalkan manfaat madu alami kepada masyarakat luas dalam penyediaan madu berkualitas tinggi. Kami percaya bahwa produk alami memiliki kekuatan untuk meningkatkan 
       kesehatan dan kesejahteraan, dan kami berkomitmen untuk menyediakan madu terbaik untuk konsumen kami.
     `,
   };
